0135136b73ac10ea2b1de2efb9f6cd69803074be
pablo <pnunfe@gmail.com>
2024-09-25 16:03:20 +0200

    Refactor routing and add wildcard redirect

--- src/app/pages/product-page/product-page.component.ts
+++ src/app/pages/product-page/product-page.component.ts
@@ -1,19 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   standalone: true,
   selector: 'app-product-page',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
 
   template: `<h4>Product Page</h4>
     <hr />
-    <form>
+    <form [formGroup]="myForm">
       <div class="row g-3 align-items-center mb-3">
         <div class="col-auto">
           <label>Nombre:</label>
         </div>
         <div class="col-auto">
-          <input type="text" class="form-control" />
+          <input formControlName="name" type="text" class="form-control" />
         </div>
         <div class="col-auto">
           <span>Algo de información</span>
@@ -21,9 +23,32 @@ import { Component } from '@angular/core';
       </div>
       <div class="row">
         <div class="col">
-          <button type="submit" class="btn btn-primary">Cambiar Color</button>
+          <button (click)="changeColor()" type="submit" class="btn btn-primary">
+            Cambiar Color
+          </button>
         </div>
       </div>
-    </form>`,
+    </form>
+    <pre class="mt-5">
+      {{ myForm.get('name')?.errors | json }}
+    </pre
+    >
+    <p>Color: {{ color }}</p>`,
 })
-export class ProductPageComponent {}
+export class ProductPageComponent {
+  readonly #formBuilder = inject(FormBuilder);
+  readonly myForm = this.#formBuilder.group({
+    name: [
+      '',
+      [Validators.required, Validators.minLength(6), Validators.email],
+    ],
+  });
+
+  public color: string = 'red';
+
+  changeColor() {
+    this.color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+  }
+}
