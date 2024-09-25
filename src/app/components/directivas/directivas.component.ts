fabdc453cfd4278878083daa0bc11d86837387d0
pablo <pnunfe@gmail.com>
2024-09-25 10:23:42 +0200

    inicial commit

---
+++ src/app/components/directivas/directivas.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-directivas',
+  imports: [],
+  template: ` <h1>Directivas</h1>`,
+})
+export class DirectivasComponent {}
